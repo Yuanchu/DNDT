b1bfb72f7193a3dc9280d2f680b2b87bac43e5a5
Yuanchu <yuanchu.dang@gmail.com>
2019-05-12 19:05:49 -0400

    Various extensions

---
+++ tensorflow/main.py
@@ -0,0 +1,56 @@
+import numpy as np
+import tensorflow as tf
+import iris
+import matplotlib.pyplot as plt
+from neural_network_decision_tree import *
+from car import *
+
+np.random.seed(1943)
+tf.set_random_seed(1943)
+
+# x, y = get_car()
+
+# Input data
+x = iris.feature[:, :]
+y = iris.label
+d = x.shape[1]
+num_class = y.shape[1]
+
+# Model params
+model_name = "new"
+cut = 2
+rotate_size = 4
+learn_rate = 0.01
+train_prob = 1.0
+test_prob = 1.0
+use_rotate = True
+use_cnn = False
+
+# Training params
+batch_size = 25
+run = int(10e3)
+
+args = {"use_rotate": use_rotate,
+        "rotate_size": rotate_size,
+        "use_cnn": use_cnn}
+
+# Define the session
+sess = tf.InteractiveSession()
+
+# Define the model
+x_ph, y_ph, keep_prob, y_pred, train_step, loss = get_model(model_name, d, cut, num_class, learn_rate, args)
+
+# Initialize the variables
+sess.run(tf.global_variables_initializer())
+
+# Train the model
+for i in range(run):
+    batch_idx = np.random.randint(0, x.shape[0], batch_size)
+    x_batch = x[batch_idx]
+    y_batch = y[batch_idx]
+    _, loss_e = sess.run([train_step, loss], feed_dict={x_ph: x_batch, y_ph: y_batch, keep_prob: train_prob})
+    if i % 200 == 0:
+        loss_all = sess.run([loss], feed_dict={x_ph: x, y_ph: y, keep_prob: test_prob})
+        print(loss_all)
+print('error rate %.2f' % (1 - np.mean(np.argmax(y_pred.eval(feed_dict={x_ph: x, keep_prob: test_prob}), axis=1) == np.argmax(y, axis=1))))
+
--- tensorflow/neural_network_decision_tree.py
+++ tensorflow/neural_network_decision_tree.py
@@ -1,5 +1,62 @@
 import tensorflow as tf
 from functools import reduce
+import numpy as np
+
+def get_model(model_name, d, cut, num_class, learn_rate,  args):
+
+    # Model has to be "old" or "new"
+    if model_name not in ['old', 'new']:
+        raise ValueError("Invalid model: %s!\n" % model_name)
+    
+    # Old model does not use rotation or CNN
+    if model_name == "old":
+        args["use_rotate"] = False
+        args["use_cnn"] = False
+    
+    # Control flags
+    use_rotate = args.get("use_rotate", True)
+    use_cnn = args.get("use_cnn", True)
+    
+    # Parameters for the new model
+    k = args.get("k", 3)
+    rotate_size = args.get("rotate_size", 5)
+    
+    # Cut on k features if use CNN
+    # Cut on rotate_size features if only use rotation
+    # Cut on d features otherwise 
+    if model_name == "new":
+        if use_cnn:
+            num_cut = [cut for _ in range(k)]
+        elif use_rotate:
+            num_cut = [cut for _ in range(rotate_size)]
+        else:
+            num_cut = [cut for _ in range(d)]
+    else:
+        num_cut = [cut for _ in range(d)]
+
+    num_leaf = np.prod(np.array(num_cut) + 1)
+
+    x_ph = tf.placeholder(tf.float32, [None, d])
+    y_ph = tf.placeholder(tf.float32, [None, num_class])
+    keep_prob = tf.placeholder(tf.float32, name = 'keep_prob')
+
+    if use_rotate:
+        W1 = tf.Variable(tf.random_uniform((d, rotate_size), -1, 1))
+        x_ph_rotated = tf.matmul(x_ph, W1)
+    else:
+        x_ph_rotated = x_ph
+
+    cut_points_list = [tf.Variable(tf.random_uniform([i])) for i in num_cut]
+    leaf_score = tf.Variable(tf.random_uniform([num_leaf, num_class]))
+
+    # import pdb; pdb.set_trace()
+    y_pred = nn_decision_tree(x_ph_rotated, cut_points_list, leaf_score, keep_prob, model_name, args, temperature = 0.1)
+    loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(logits = y_pred, onehot_labels = y_ph))
+
+    opt = tf.train.AdamOptimizer(learn_rate)
+    train_step = opt.minimize(loss)
+
+    return x_ph, y_ph, keep_prob, y_pred, train_step, loss
 
 
 def tf_kron_prod(a, b):
@@ -8,21 +65,50 @@ def tf_kron_prod(a, b):
     return res
 
 
-def tf_bin(x, cut_points, temperature=0.1):
-    # x is a N-by-1 matrix (column vector)
-    # cut_points is a D-dim vector (D is the number of cut-points)
-    # this function produces a N-by-(D+1) matrix, each row has only one element being one and the rest are all zeros
+def tf_bin(x, cut_points, hidden = 2, temperature=0.1):
+    """
+    x is a N-by-1 matrix (column vector)
+    cut_points is a D-dim vector (D is the number of cut-points)
+    this function produces a N-by-(D+1) matrix, each row has only one element being one and the rest are all zeros
+    """
     D = cut_points.get_shape().as_list()[0]
     W = tf.reshape(tf.linspace(1.0, D + 1.0, D + 1), [1, -1])
     cut_points = tf.contrib.framework.sort(cut_points)  # make sure cut_points is monotonically increasing
     b = tf.cumsum(tf.concat([tf.constant(0.0, shape=[1]), -cut_points], 0))
     h = tf.matmul(x, W) + b
+
     res = tf.nn.softmax(h / temperature)
     return res
 
 
-def nn_decision_tree(x, cut_points_list, leaf_score, temperature=0.1):
+def nn_decision_tree_old(x, cut_points_list, leaf_score, keep_prob, temperature=0.1):
     # cut_points_list contains the cut_points for each dimension of feature
-    leaf = reduce(tf_kron_prod,
-                  map(lambda z: tf_bin(x[:, z[0]:z[0] + 1], z[1], temperature), enumerate(cut_points_list)))
-    return tf.matmul(leaf, leaf_score)
+    binnings = list(map(lambda z: tf_bin(x[:, z[0]:z[0] + 1], z[1], temperature = temperature), enumerate(cut_points_list)))
+    leaf = reduce(tf_kron_prod, binnings)
+    leaf_dropped = tf.nn.dropout(leaf, keep_prob)
+    return tf.matmul(leaf_dropped, leaf_score)
+
+
+def nn_decision_tree(x, cut_points_list, leaf_score, keep_prob, model_name, args, temperature=0.1):
+
+    # If use old model, fall back to the old method
+    if model_name == "old":
+        return nn_decision_tree_old(x, cut_points_list, leaf_score, keep_prob, temperature = temperature)
+    else:
+        # Add optional CNN layer
+        use_cnn = args.get("use_cnn", True)
+        if use_cnn:
+            k = args.get("k", 3)
+            d = x.shape[1]
+            reshaped = tf.reshape(x, [-1, 1, d])
+            conv1 = tf.layers.conv1d(inputs=reshaped, filters=k, kernel_size=2, padding="same", activation=tf.nn.softmax)
+            binning_input = tf.reshape(conv1, [-1, k])
+        else:
+            binning_input = x
+        
+        # Same kron product and dropout
+        binnings = list(map(lambda z: tf_bin(binning_input[:, z[0]:z[0] + 1], z[1], temperature = temperature),
+            enumerate(cut_points_list)))        
+        leaf = reduce(tf_kron_prod, binnings)
+        leaf_dropped = tf.nn.dropout(leaf, keep_prob)
+        return tf.matmul(leaf_dropped, leaf_score)
